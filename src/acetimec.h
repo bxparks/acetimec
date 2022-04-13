e214044ea026b32f6e8dd6fbde7a3eebcb24cb9c
Brian Park <brian@xparks.net>
2022-04-13 11:25:49 -0700

    src/zonedb: Generate zonedb files using AceTimeTools/tzcompiler.py

--- src/acetimec.h
+++ src/acetimec.h
@@ -8,5 +8,8 @@
 
 #include "ace_time_c/local_date.h"
 #include "ace_time_c/local_date_time.h"
+#include "ace_time_c/zonedb/zone_infos.h"
+#include "ace_time_c/zonedb/zone_policies.h"
+#include "ace_time_c/zonedb/zone_registry.h"
 
 #endif
