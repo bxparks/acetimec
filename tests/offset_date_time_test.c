0c7a35e194ce141dd7c5561d0c5e2efc0324e637
Brian Park <brian@xparks.net>
2022-08-03 15:20:05 -0700

    tests/offset_date_time_test.c: Add tests for offset_date_time.c

---
+++ tests/offset_date_time_test.c
@@ -0,0 +1,82 @@
+#include "acunit.h"
+#include <acetimec.h>
+
+ACU_TEST(test_offset_date_time_to_epoch_seconds)
+{
+  // smallest valid offset_date_time
+  {
+    struct AtcOffsetDateTime odt = {
+      1931, 12, 13, 20, 45, 53, 0 /*fold*/, 0 /*offset*/
+    };
+    ACU_ASSERT(INT32_MIN + 1 == atc_offset_date_time_to_epoch_seconds(&odt));
+  }
+
+  // offset_date_time at exactly epoch_seconds==0
+  {
+    struct AtcOffsetDateTime odt = {
+      2000, 1, 1, 0, 0, 0, 0 /*fold*/, 0 /*offset*/
+    };
+    ACU_ASSERT(0 == atc_offset_date_time_to_epoch_seconds(&odt));
+  }
+
+  // one day later
+  {
+    struct AtcOffsetDateTime odt = {
+      2000, 1, 2, 0, 0, 0, 0 /*fold*/, 0 /*offset*/
+    };
+    ACU_ASSERT(86400 == atc_offset_date_time_to_epoch_seconds(&odt));
+  }
+
+  // largest value using Unix Epoch
+  {
+    struct AtcOffsetDateTime odt = {
+      2038, 1, 19, 3, 14, 7, 0 /*fold*/, 0 /*offset*/
+    };
+    ACU_ASSERT(1200798847 == atc_offset_date_time_to_epoch_seconds(&odt));
+  }
+
+  // largest valid offset_date_time given 32-bit epoch_seconds
+  {
+    struct AtcOffsetDateTime odt = {
+      2068, 1, 19, 3, 14, 7, 0 /*fold*/, 0 /*offset*/
+    };
+    ACU_ASSERT(INT32_MAX == atc_offset_date_time_to_epoch_seconds(&odt));
+  }
+
+  ACU_PASS();
+}
+
+ACU_TEST(test_offset_date_time_to_epoch_seconds_with_offset)
+{
+  uint16_t offset_minutes = 1;
+  struct AtcOffsetDateTime odt = {
+    2000, 1, 1, 0, 0, 0, 0 /*fold*/, offset_minutes /*offset*/
+  };
+  ACU_ASSERT(offset_minutes * -60
+      == atc_offset_date_time_to_epoch_seconds(&odt));
+
+  ACU_PASS();
+}
+
+ACU_TEST(test_offset_date_time_to_epoch_seconds_invalid)
+{
+  struct AtcOffsetDateTime odt = {
+    kAtcInvalidYear, 12, 13, 20, 45, 53, 0 /*fold*/, 0 /*offset*/
+  };
+  ACU_ASSERT(kAtcInvalidEpochSeconds
+      == atc_offset_date_time_to_epoch_seconds(&odt));
+  
+  ACU_PASS();
+}
+
+//---------------------------------------------------------------------------
+
+ACU_PARAMS();
+
+int main()
+{
+  ACU_RUN_TEST(test_offset_date_time_to_epoch_seconds);
+  ACU_RUN_TEST(test_offset_date_time_to_epoch_seconds_with_offset);
+  ACU_RUN_TEST(test_offset_date_time_to_epoch_seconds_invalid);
+  ACU_SUMMARY();
+}
