6b7052a6df9446531c7b2c9c77b742449625e2d7
Brian Park <brian@xparks.net>
2022-09-01 14:22:20 -0700

    src/string_buffer.h: Add docstrings to eliminate doxygen warnings

--- src/ace_time_c/string_buffer.h
+++ src/ace_time_c/string_buffer.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/** An array of characters that knows its capacity and its current usage size */
 typedef struct AtcStringBuffer {
   /** Pointer to base of the string buffer. */
   char *p;
@@ -19,20 +20,28 @@ typedef struct AtcStringBuffer {
   uint8_t size;
 } AtcStringBuffer;
 
+/** Initialize the AtcStringBuffer with its char pointer and capacity. */
 void atc_print_init(AtcStringBuffer *sb, char *p, uint8_t capacity);
 
+/** Finalize the AtcStringBuffer by terminating the string with a NUL. */
 void atc_print_end(AtcStringBuffer *sb);
 
+/** Print one char into the AtcStringBuffer. */
 void atc_print_char(AtcStringBuffer *sb, char c);
 
+/** Print the given string into the AtcStringBuffer. */
 void atc_print_string(AtcStringBuffer *sb, const char *s);
 
+/** Print the human-readable digits of uint16_t into the AtcStringBuffer. */
 void atc_print_uint16(AtcStringBuffer *sb, uint16_t n);
 
+/** Print the unsigned integer padded to 2 digits. 1 is printed as "01". */
 void atc_print_uint16_pad2(AtcStringBuffer *sb, uint16_t n);
 
+/** Print the unsigned integer padded to 4 digits. 12 is printed as "0012". */
 void atc_print_uint16_pad4(AtcStringBuffer *sb, uint16_t n);
 
+/** Print the signed integer. A negative number will print a leading "-". */
 void atc_print_int16(AtcStringBuffer *sb, int16_t n);
 
 #endif
