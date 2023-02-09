d42f052e35fb7a6af78a0d6a9a26c8bec341074f
Brian Park <brian@xparks.net>
2023-02-08 16:57:39 -0800

    multizone: Update comments for MacOS which seems to fail the invalid detection logic

--- examples/multizone/multizone.c
+++ examples/multizone/multizone.c
@@ -8,13 +8,14 @@
 #include <time.h> // time(), localtime_r()
 #include <stdlib.h> // setenv()
 #include <stdbool.h> // bool
-#include <unistd.h> // sleep
+#include <unistd.h> // sleep()
 
 /**
  * Set the timezone as currently specified by the "TZ" environment variable.
- * Returns true if the time zone is valid, false otherwise.
+ * Returns 0 if the time zone is valid, 1 otherwise.
  */
-bool set_time_zone(const char *zone_name) {
+int set_time_zone(const char *zone_name)
+{
   setenv("TZ", zone_name, 1 /*overwrite*/);
 
   // Update the following **global** variables:
@@ -29,20 +30,27 @@ bool set_time_zone(const char *zone_name) {
   // (What a mess.)
   tzset();
 
-  // tzset() does not set an error status, so we don't know if the ZONE_NAME is
-  // valid or not. So we use the following heuristics: If the zone does not
-  // exist, then tzset() will set the zone to UTC, so daylight offset will be
-  // 0. But there are legitimate timezones which track UTC. But when the zone
-  // is invalid, it seems like tzname[0] is set to a truncated version of the
-  // original zone name, and tzname[1] is set to an empty string.
+  // tzset() does not set an error status. If the zone does not exist, then
+  // tzset() will set the zone to UTC, so daylight offset will be 0. How do we
+  // know if the requested zone exists in the database?
+  //
+  // The following heuristics works on Linux:
+  //  * tzname[0] == atruncated version of the original zone name, and
+  //  * tzname[1] == ""
+  //
+  // On MacOS, both "UTC" and an invalid zone returns the following:
+  //  * tzname[0] == "UTC"
+  //  * tzname[1] == "   "
+  // so we cannot tell if the zone is valid or not.
   bool invalid = strncmp(tzname[0], zone_name, strlen(tzname[0])) == 0
       && tzname[1][0] == '\0';
 
-  return !invalid;
+  return invalid;
 }
 
-const char * const ZONES[] ={
+const char * const ZONES[] = {
   "UTC",
+  "DoesNotExist",
   "America/Los_Angeles",
   "America/New_York",
   "Europe/London",
@@ -51,7 +59,8 @@ const char * const ZONES[] ={
 
 #define NUM_ZONES (sizeof(ZONES) / sizeof(ZONES[0]))
 
-void print_time(time_t epoch_seconds) {
+void print_time(time_t epoch_seconds)
+{
   struct tm tms;
   localtime_r(&epoch_seconds, &tms);
 
@@ -63,27 +72,23 @@ void print_time(time_t epoch_seconds) {
   int second = tms.tm_sec;
   //int gmtoff = tms.tm_gmtoff;
 
-  printf("%04d-%02d-%02dT%02d:%02d:%02d\n",
+  printf("%04d-%02d-%02dT%02d:%02d:%02d",
       year, month, day, hour, minute, second);
 }
 
-int main() {
-
-  for (int n = 0; n < 1000; n++) {
-    //time_t epoch_seconds = 1675209600; // 2023-02-01T00:00:00 UTC
-    time_t epoch_seconds = time(NULL);
-    for (int i = 0; i < (int)NUM_ZONES; i++) {
-      const char *name = ZONES[i];
-      bool ok = set_time_zone(name);
-      if (ok) {
-        printf("Zone %s: ", name);
-        print_time(epoch_seconds);
-      } else {
-        printf("ERROR: Invalid zone %s\n", name);
-      }
+int main()
+{
+  //time_t epoch_seconds = 1675209600; // 2023-02-01T00:00:00 UTC
+  time_t epoch_seconds = time(NULL);
+  for (int i = 0; i < (int)NUM_ZONES; i++) {
+    const char *name = ZONES[i];
+    int err = set_time_zone(name);
+    if (err) {
+      printf("ERROR: Invalid zone %s\n", name);
+    } else {
+      printf("Zone %s: tzname[]={\"%s\",\"%s\"}: ", name, tzname[0], tzname[1]);
+      print_time(epoch_seconds);
+      printf("\n");
     }
-    printf("\n");
-    sleep(2);
   }
 }
-
