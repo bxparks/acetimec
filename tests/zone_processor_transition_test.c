b2d4cb3714a20c77e39393482856ffe1542f8d99
Brian Park <brian@xparks.net>
2023-03-26 16:03:41 -0700

    tests: Add units for new zonedb [2000,10000)

--- tests/zone_processor_transition_test.c
+++ tests/zone_processor_transition_test.c
@@ -63,7 +63,7 @@ static void validate_zone(
   }
 }
 
-ACU_TEST(test_transitions_for_all_zones_all_years) {
+ACU_TEST(test_transitions_for_zonedball) {
   int16_t saved_epoch_year = atc_get_current_epoch_year();
   AtcZoneProcessor processor;
   atc_processor_init(&processor);
@@ -71,11 +71,11 @@ ACU_TEST(test_transitions_for_all_zones_all_years) {
   for (uint16_t i = 0; i < kAtcAllZoneRegistrySize; i++) {
     const AtcZoneInfo *info = kAtcAllZoneRegistry[i];
 
-    // Loop from start_year to until_year, in chunks of 100 years, shifting
+    // Loop from [start_year, 3000), in chunks of 100 years, shifting
     // the current_epoch_year by 100 years.
     for (
         int16_t start_year = kAtcAllZoneContext.start_year;
-        start_year < kAtcAllZoneContext.until_year;
+        start_year < 3000;
         start_year += 100) {
 
       atc_set_current_epoch_year(start_year + 50);
@@ -93,12 +93,43 @@ ACU_TEST(test_transitions_for_all_zones_all_years) {
   atc_set_current_epoch_year(saved_epoch_year);
 }
 
+ACU_TEST(test_transitions_for_zonedb) {
+  int16_t saved_epoch_year = atc_get_current_epoch_year();
+  AtcZoneProcessor processor;
+  atc_processor_init(&processor);
+
+  for (uint16_t i = 0; i < kAtcZoneRegistrySize; i++) {
+    const AtcZoneInfo *info = kAtcZoneRegistry[i];
+
+    // Loop from [start_year, 3000), in chunks of 100 years, shifting
+    // the current_epoch_year by 100 years.
+    for (
+        int16_t start_year = kAtcZoneContext.start_year;
+        start_year < 3000;
+        start_year += 100) {
+
+      atc_set_current_epoch_year(start_year + 50);
+      int16_t until_year = start_year + 100;
+      if (until_year > kAtcZoneContext.until_year) {
+        until_year = kAtcZoneContext.until_year;
+      }
+
+      ACU_ASSERT_NO_FATAL_FAILURE(
+          validate_zone(
+              acu_context, &processor, info, start_year, until_year));
+    }
+  }
+
+  atc_set_current_epoch_year(saved_epoch_year);
+}
+
 //---------------------------------------------------------------------------
 
 ACU_CONTEXT();
 
 int main()
 {
-  ACU_RUN_TEST(test_transitions_for_all_zones_all_years);
+  ACU_RUN_TEST(test_transitions_for_zonedb);
+  ACU_RUN_TEST(test_transitions_for_zonedball);
   ACU_SUMMARY();
 }
--- tests/zonedb_test.c
+++ tests/zonedb_test.c
@@ -4,9 +4,15 @@
 ACU_TEST(test_zonedb_sizes)
 {
   // These numbers are correct for TZDB 2022g
+  ACU_ASSERT(sizeof(kAtcZoneRegistry) / sizeof(AtcZoneInfo*) == 351);
+  ACU_ASSERT(sizeof(kAtcZoneAndLinkRegistry) / sizeof(AtcZoneInfo*) == 596);
+}
+
+ACU_TEST(test_zonedball_sizes)
+{
+  // These numbers are correct for TZDB 2022g
   ACU_ASSERT(sizeof(kAtcAllZoneRegistry) / sizeof(AtcZoneInfo*) == 351);
-  ACU_ASSERT(sizeof(kAtcAllZoneAndLinkRegistry) / sizeof(AtcZoneInfo*)
-      == 596);
+  ACU_ASSERT(sizeof(kAtcAllZoneAndLinkRegistry) / sizeof(AtcZoneInfo*) == 596);
 }
 
 //---------------------------------------------------------------------------
@@ -16,5 +22,6 @@ ACU_CONTEXT();
 int main()
 {
   ACU_RUN_TEST(test_zonedb_sizes);
+  ACU_RUN_TEST(test_zonedball_sizes);
   ACU_SUMMARY();
 }
